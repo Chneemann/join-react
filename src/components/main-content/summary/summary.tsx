7a2781cb1cc0aa33d4ad6710601b340a149ef65f
Chneemann <andre-kempf@mailbox.org>
2024-09-25 21:20:51 +0200

    Firebase integrated, service for database query created & display of data in urgent-task

--- src/components/main-content/summary/summary.tsx
+++ src/components/main-content/summary/summary.tsx
@@ -1,132 +1,162 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchTasks } from "../../../services/firebase";
 import "./summary.css";
 
-interface SummaryProps {}
+const Summary: React.FC = () => {
+  const [tasks, setUsers] = useState<any[]>([]);
 
-interface SummaryState {}
+  useEffect(() => {
+    const loadUsers = async () => {
+      try {
+        const tasksList = await fetchTasks();
+        setUsers(tasksList);
+      } catch (error) {
+        console.error("Error loading tasks:", error);
+      }
+    };
+    loadUsers();
+  }, []);
 
-class Summary extends React.Component<SummaryProps, SummaryState> {
-  state = {};
-  render() {
-    return (
-      <div className="summary">
-        <div className="headline">
-          Good morning, <span>Guest</span>
-        </div>
-        <div className="content">
-          <div className="content-container-upper">
-            <div className="urgent-task">
-              <div className="urgent-task-container">
-                <div className="urgent-task-info">
-                  <div className="urgent-task-icon">
-                    <img
-                      src="./../assets/img/summary/urgent.svg"
-                      alt="Urgent task icon"
-                    />
-                    <span>1</span>
-                  </div>
-                  <p>Tasks Urgent</p>
+  // Count the number of “urgent” tasks
+  const urgentTasks = tasks.filter((task) => task.priority === "urgent");
+  const urgentTasksCount = urgentTasks.length;
+
+  // Find the earliest due date among the “urgent” tasks
+  const earliestDueDate = urgentTasks
+    .map((task) => new Date(task.date))
+    .sort((a, b) => a.getTime() - b.getTime())[0];
+
+  // Convert the earliest due date into a readable format
+  const formatDueDate = (date: Date) => {
+    const options: Intl.DateTimeFormatOptions = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    return date.toLocaleDateString("en-US", options);
+  };
+  const formattedDueDate = earliestDueDate
+    ? formatDueDate(earliestDueDate)
+    : "No Deadline";
+
+  return (
+    <div className="summary">
+      <div className="headline">
+        Good morning, <span>Guest</span>
+      </div>
+      <div className="content">
+        <div className="content-container-upper">
+          <div className="urgent-task">
+            <div className="urgent-task-container">
+              <div className="urgent-task-info">
+                <div className="urgent-task-icon">
+                  <img
+                    src="./../assets/img/summary/urgent.svg"
+                    alt="Urgent task icon"
+                  />
+                  <span>{urgentTasksCount}</span>
                 </div>
-                <div className="urgent-task-divider"></div>
-                <div className="urgent-task-deadline">
-                  <span>October 16, 2022</span>
-                  <p>Upcoming Deadline</p>
-                </div>
+                <p>Tasks Urgent</p>
               </div>
-            </div>
-            <div className="task-in-board">
-              <div className="task-in-board-container">
-                <div className="task-in-board-info">
-                  <div className="task-in-board-icon">
-                    <img
-                      src="./../assets/img/summary/board.svg"
-                      alt="Board task icon"
-                    />
-                    <span>1</span>
-                  </div>
-                  <p>
-                    Tasks in
-                    <br />
-                    Board
-                  </p>
-                </div>
+              <div className="urgent-task-divider"></div>
+              <div className="urgent-task-deadline">
+                <span>{formattedDueDate}</span>
+                <p>Upcoming Deadline</p>
               </div>
             </div>
           </div>
-          <div className="content-container-lower">
-            <div className="task-todo">
-              <div className="task-todo-container">
-                <div className="task-todo-info">
-                  <div className="task-todo-icon">
-                    <img
-                      src="./../assets/img/summary/todo.svg"
-                      alt="Todo task icon"
-                    />
-                    <span>1</span>
-                  </div>
-                  <p>Tasks To-do</p>
+          <div className="task-in-board">
+            <div className="task-in-board-container">
+              <div className="task-in-board-info">
+                <div className="task-in-board-icon">
+                  <img
+                    src="./../assets/img/summary/board.svg"
+                    alt="Board task icon"
+                  />
+                  <span>1</span>
                 </div>
+                <p>
+                  Tasks in
+                  <br />
+                  Board
+                </p>
               </div>
             </div>
-            <div className="task-other">
-              <div className="task-other-container">
-                <div className="task-other-info">
-                  <div className="task-other-icon">
-                    <img
-                      src="./../assets/img/summary/in-progress.svg"
-                      alt="Todo task icon"
-                    />
-                    <span>1</span>
-                  </div>
-                  <p>
-                    Tasks in
-                    <br />
-                    Progress
-                  </p>
+          </div>
+        </div>
+        <div className="content-container-lower">
+          <div className="task-todo">
+            <div className="task-todo-container">
+              <div className="task-todo-info">
+                <div className="task-todo-icon">
+                  <img
+                    src="./../assets/img/summary/todo.svg"
+                    alt="Todo task icon"
+                  />
+                  <span>1</span>
                 </div>
+                <p>Tasks To-do</p>
               </div>
             </div>
-            <div className="task-other">
-              <div className="task-other-container">
-                <div className="task-other-info">
-                  <div className="task-other-icon">
-                    <img
-                      src="./../assets/img/summary/awaiting-feedback.svg"
-                      alt="Todo task icon"
-                    />
-                    <span>1</span>
-                  </div>
-                  <p>
-                    Awaiting
-                    <br />
-                    Feedback
-                  </p>
+          </div>
+          <div className="task-other">
+            <div className="task-other-container">
+              <div className="task-other-info">
+                <div className="task-other-icon">
+                  <img
+                    src="./../assets/img/summary/in-progress.svg"
+                    alt="Todo task icon"
+                  />
+                  <span>1</span>
                 </div>
+                <p>
+                  Tasks in
+                  <br />
+                  Progress
+                </p>
               </div>
             </div>
-            <div className="task-done">
-              <div className="task-done-container">
-                <div className="task-done-info">
-                  <div className="task-done-icon">
-                    <img
-                      src="./../assets/img/summary/done.svg"
-                      alt="Todo task icon"
-                    />
-                    <span>1</span>
-                  </div>
-                  <p>
-                    Tasks
-                    <br />
-                    Done
-                  </p>
+          </div>
+          <div className="task-other">
+            <div className="task-other-container">
+              <div className="task-other-info">
+                <div className="task-other-icon">
+                  <img
+                    src="./../assets/img/summary/awaiting-feedback.svg"
+                    alt="Todo task icon"
+                  />
+                  <span>1</span>
                 </div>
+                <p>
+                  Awaiting
+                  <br />
+                  Feedback
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="task-done">
+            <div className="task-done-container">
+              <div className="task-done-info">
+                <div className="task-done-icon">
+                  <img
+                    src="./../assets/img/summary/done.svg"
+                    alt="Todo task icon"
+                  />
+                  <span>1</span>
+                </div>
+                <p>
+                  Tasks
+                  <br />
+                  Done
+                </p>
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Summary;
