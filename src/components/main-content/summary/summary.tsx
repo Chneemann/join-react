ce59ecf403c7116136fd3bd4a2dae7805ee0e9bf
Chneemann <andre-kempf@mailbox.org>
2024-09-25 10:08:49 +0200

    Added: Basic framework and animations for the “summary” component

--- src/components/main-content/summary/summary.tsx
+++ src/components/main-content/summary/summary.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./summary.css";
 
 interface SummaryProps {}
 
@@ -8,8 +9,22 @@ class Summary extends React.Component<SummaryProps, SummaryState> {
   state = {};
   render() {
     return (
-      <div>
-        <h1>Summary</h1>
+      <div className="summary">
+        <div className="headline">
+          Good morning, <span>Guest</span>
+        </div>
+        <div className="content">
+          <div className="content-container-upper">
+            <div className="urgent-tasks"></div>
+            <div className="task-in-board"></div>
+          </div>
+          <div className="content-container-lower">
+            <div className="task-todo"></div>
+            <div className="task-container"></div>
+            <div className="task-container"></div>
+            <div className="task-done"></div>
+          </div>
+        </div>
       </div>
     );
   }
