e955a56e708d106262f275f3991a0d57a80720ce
Chneemann <andre-kempf@mailbox.org>
2024-09-29 16:21:28 +0200

    Design and implement Board component structure

--- src/components/main-content/board.tsx
+++ src/components/main-content/board.tsx
@@ -1,15 +1,96 @@
-import React from "react";
+import React, { Component } from "react";
+import "./board.css";
+import { Task } from "../../interfaces/task.interface";
 
-interface BoardProps {}
+interface State {
+  searchValue: string;
+  tasks: Task[];
+}
 
-interface BoardState {}
+class Board extends Component<{}, State> {
+  searchInput: HTMLInputElement | null = null;
 
-class Board extends React.Component<BoardProps, BoardState> {
-  state = {};
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      searchValue: "",
+      tasks: [],
+    };
+  }
+
+  clearInput = () => {
+    this.setState({ searchValue: "" });
+  };
+
   render() {
+    const { searchValue } = this.state;
+
+    const statusDisplayNames: { [key: string]: string } = {
+      todo: "To-do",
+      inprogress: "In Progress",
+      awaitfeedback: "Await Feedback",
+      done: "Done",
+    };
+
     return (
-      <div>
-        <h1>Board</h1>
+      <div className="board">
+        <div className="header">
+          <div className="title">{"Board"}</div>
+          <div className="search">
+            <div className="search-inner">
+              <input
+                ref={(input) => (this.searchInput = input)}
+                id="search-task"
+                type="text"
+                placeholder="Find Task"
+                value={searchValue}
+                onChange={(e) => this.setState({ searchValue: e.target.value })}
+              />
+              <span>
+                {searchValue ? (
+                  <img
+                    src="./../../../assets/img/board/clear.svg"
+                    className="icon-clear"
+                    alt="clear"
+                    onClick={this.clearInput}
+                  />
+                ) : (
+                  <img
+                    src="./../../../assets/img/board/search.svg"
+                    className="icon-search"
+                    alt="search"
+                  />
+                )}
+              </span>
+              <span className="line"></span>
+            </div>
+            <button className="btn" type="submit">
+              <div className="btn-inside">
+                <span>Add Task</span>
+                <img
+                  src="./../../../assets/img/board/add_white.svg"
+                  alt="check"
+                />
+              </div>
+            </button>
+          </div>
+        </div>
+        <div className="content">
+          <div className="status">
+            {["todo", "inprogress", "awaitfeedback", "done"].map((status) => (
+              <div key={status} className="column">
+                <div className="headline">
+                  <span>{statusDisplayNames[status]}</span>
+                  <img src="./../../../assets/img/board/plus.svg" alt="add" />
+                </div>
+                <div id={status} className="details">
+                  <div>Task Placeholder</div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div id="content-tasks"></div>
+        </div>
       </div>
     );
   }
