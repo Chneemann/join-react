704ad5d34f30c64fa0629d45df2eeb73813aeb81
Chneemann <andre-kempf@mailbox.org>
2024-10-06 20:21:11 +0200

    Implement title input fields with validation and error handling in "Add Task" component

--- src/components/main-content/add-task.tsx
+++ src/components/main-content/add-task.tsx
@@ -1,15 +1,75 @@
-import React from "react";
+import React, { Component } from "react";
+import "./add-task.css";
 
-interface AddTaskProps {}
+interface TaskData {
+  title: string;
+}
 
-interface AddTaskState {}
+interface Props {}
 
-class AddTask extends React.Component<AddTaskProps, AddTaskState> {
-  state = {};
+interface State {
+  taskData: TaskData;
+  titleTouched: boolean;
+  titleError: string;
+}
+
+class AddTask extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      taskData: {
+        title: "",
+      },
+      titleTouched: false,
+      titleError: "",
+    };
+  }
+
+  handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      taskData: {
+        ...this.state.taskData,
+        title: event.target.value,
+      },
+      titleTouched: true,
+    });
+  };
+
   render() {
+    const { taskData, titleTouched, titleError } = this.state;
+
+    const isTitleValid = taskData.title.length >= 8;
+    const isTitleEmpty = taskData.title.length === 0;
+
     return (
-      <div>
-        <h1>Add Task</h1>
+      <div className="title">
+        <p>
+          Title
+          <span className="red-dot">*</span>
+        </p>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          placeholder={taskData.title ? taskData.title : "test"}
+          value={taskData.title}
+          autoComplete="off"
+          required
+          onChange={this.handleInputChange}
+          onBlur={() => {
+            this.setState({
+              titleTouched: true,
+              titleError: isTitleEmpty
+                ? "Required"
+                : !isTitleValid
+                ? "Minimum 8 letters required"
+                : "",
+            });
+          }}
+        />
+        <div className="error-msg">
+          {titleTouched && titleError && <p>{titleError}</p>}
+        </div>
       </div>
     );
   }
