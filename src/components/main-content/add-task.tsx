95814357b848eb18b2b57d09f32609b2889aa76a
Chneemann <andre-kempf@mailbox.org>
2024-10-06 20:24:28 +0200

    Implement description textfield with validation and error handling in "Add Task" component

--- src/components/main-content/add-task.tsx
+++ src/components/main-content/add-task.tsx
@@ -1,27 +1,30 @@
-import React, { Component } from "react";
-import "./add-task.css";
+import React from "react";
 
 interface TaskData {
   title: string;
+  description: string;
 }
-
-interface Props {}
 
 interface State {
   taskData: TaskData;
   titleTouched: boolean;
   titleError: string;
+  descriptionTouched: boolean;
+  descriptionError: string;
 }
 
-class AddTask extends Component<Props, State> {
-  constructor(props: Props) {
+class TaskForm extends React.Component<{}, State> {
+  constructor(props: {}) {
     super(props);
     this.state = {
       taskData: {
         title: "",
+        description: "",
       },
       titleTouched: false,
       titleError: "",
+      descriptionTouched: false,
+      descriptionError: "",
     };
   }
 
@@ -31,48 +34,101 @@ class AddTask extends Component<Props, State> {
         ...this.state.taskData,
         title: event.target.value,
       },
-      titleTouched: true,
+      titleTouched: false,
+    });
+  };
+
+  handleDescriptionChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    this.setState({
+      taskData: {
+        ...this.state.taskData,
+        description: event.target.value,
+      },
+      descriptionTouched: false,
     });
   };
 
   render() {
-    const { taskData, titleTouched, titleError } = this.state;
+    const {
+      taskData,
+      titleTouched,
+      titleError,
+      descriptionTouched,
+      descriptionError,
+    } = this.state;
 
+    // Validation logic
     const isTitleValid = taskData.title.length >= 8;
     const isTitleEmpty = taskData.title.length === 0;
 
+    const isDescriptionValid = taskData.description.length >= 24;
+    const isDescriptionEmpty = taskData.description.length === 0;
+
     return (
-      <div className="title">
-        <p>
-          Title
-          <span className="red-dot">*</span>
-        </p>
-        <input
-          type="text"
-          id="title"
-          name="title"
-          placeholder={taskData.title ? taskData.title : "test"}
-          value={taskData.title}
-          autoComplete="off"
-          required
-          onChange={this.handleInputChange}
-          onBlur={() => {
-            this.setState({
-              titleTouched: true,
-              titleError: isTitleEmpty
-                ? "Required"
-                : !isTitleValid
-                ? "Minimum 8 letters required"
-                : "",
-            });
-          }}
-        />
-        <div className="error-msg">
-          {titleTouched && titleError && <p>{titleError}</p>}
+      <div>
+        <div className="title">
+          <p>
+            Title
+            <span className="red-dot">*</span>
+          </p>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            placeholder={taskData.title ? taskData.title : "Enter title..."}
+            value={taskData.title}
+            autoComplete="off"
+            required
+            onChange={this.handleInputChange}
+            onBlur={() => {
+              this.setState({
+                titleTouched: true,
+                titleError: isTitleEmpty
+                  ? "Required"
+                  : !isTitleValid
+                  ? "Minimum 8 letters required"
+                  : "",
+              });
+            }}
+          />
+          <div className="error-msg">
+            {titleTouched && titleError && <p>{titleError}</p>}
+          </div>
+        </div>
+        <div className="description">
+          <p>
+            Description
+            <span className="red-dot">*</span>
+          </p>
+          <textarea
+            id="description"
+            rows={5}
+            name="description"
+            value={taskData.description}
+            onChange={this.handleDescriptionChange}
+            onBlur={() => {
+              this.setState({
+                descriptionTouched: true,
+                descriptionError: isDescriptionEmpty
+                  ? "Required"
+                  : !isDescriptionValid
+                  ? "Minimum 24 letters required"
+                  : "",
+              });
+            }}
+            placeholder="Enter a description..."
+            autoComplete="off"
+            required
+          ></textarea>
+          <div className="error-msg">
+            {descriptionTouched && descriptionError && (
+              <p>{descriptionError}</p>
+            )}
+          </div>
         </div>
       </div>
     );
   }
 }
 
-export default AddTask;
+export default TaskForm;
