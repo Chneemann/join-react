9f01bea665f2c554602f4cb432071cb21bf4bde8
Chneemann <andre-kempf@mailbox.org>
2024-10-26 21:46:53 +0200

    design: add header and translations to ForgotPasswordComponent

--- src/components/auth/forgot-pw.tsx
+++ src/components/auth/forgot-pw.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./forgot-pw.css";
 import { withTranslation, WithTranslation } from "react-i18next";
+import SmallBtn from "../shared/components/buttons/small-btn";
 
 interface ForgotPasswordProps extends WithTranslation {}
 
@@ -13,7 +14,21 @@ class ForgotPassword extends React.Component<
   render() {
     const { t } = this.props;
 
-    return <div className="forgot-pw"></div>;
+    return (
+      <div className="forgot-pw">
+        {/* Header */}
+        <div className="forgot-pw-header">
+          <div className="forgot-pw-headline">
+            <SmallBtn image="back.svg" to="/login"></SmallBtn>
+            <div className="forgot-pw-title">{t("forgotPw.forgot")}</div>
+            <div className="forgot-pw-spacer"></div>
+          </div>
+          <div className="forgot-pw-line">
+            <img src="./../../../assets/img/auth/line.svg" alt="" />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
