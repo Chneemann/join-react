f37d756f7d157661a051dbc227678055717fa655
Chneemann <andre-kempf@mailbox.org>
2024-10-26 21:57:42 +0200

    design: add and style email input field in ForgotPasswordComponent

--- src/components/auth/forgot-pw.tsx
+++ src/components/auth/forgot-pw.tsx
@@ -5,14 +5,55 @@ import SmallBtn from "../shared/components/buttons/small-btn";
 
 interface ForgotPasswordProps extends WithTranslation {}
 
-interface ForgotPasswordState {}
+interface ForgotPasswordState {
+  email: string;
+  errorEmail: string | null;
+  isEmailValid: boolean;
+}
 
 class ForgotPassword extends React.Component<
   ForgotPasswordProps,
   ForgotPasswordState
 > {
+  constructor(props: ForgotPasswordProps) {
+    super(props);
+    this.state = {
+      email: "",
+      errorEmail: null,
+      isEmailValid: true,
+    };
+  }
+
+  // Handle form input changes
+  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    this.setState(
+      (prevState) => ({
+        ...prevState,
+        [name]: value,
+      }),
+      () => {
+        this.validateField(name, value);
+      }
+    );
+  };
+
+  validateField(name: string, value: string) {
+    const { t } = this.props;
+
+    if (name === "email") {
+      const isEmailValid = /\S+@\S+\.\S+/.test(value);
+      this.setState({
+        isEmailValid,
+        errorEmail: isEmailValid ? null : t("register.errorMailPattern"),
+      });
+    }
+  }
+
   render() {
     const { t } = this.props;
+    const { email, errorEmail } = this.state;
 
     return (
       <div className="forgot-pw">
@@ -26,6 +67,24 @@ class ForgotPassword extends React.Component<
           <div className="forgot-pw-line">
             <img src="./../../../assets/img/auth/line.svg" alt="" />
           </div>
+        </div>
+        <div className="forgot-pw-input-fields">
+          {/* Email Field */}
+          <input
+            type="email"
+            name="email"
+            autoComplete="email"
+            placeholder={t("register.email")}
+            value={email}
+            onChange={this.handleChange}
+            required
+          />
+          <img
+            className="forgot-pw-icon-mail"
+            src="./../../../assets/img/auth/mail.svg"
+            alt="mail"
+          />
+          <div className="error-msg">{errorEmail && <p>{errorEmail}</p>}</div>
         </div>
       </div>
     );
