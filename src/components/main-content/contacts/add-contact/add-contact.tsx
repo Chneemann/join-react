e7e9d358afe8e0909ce080f34c7b8774b26714dd
Chneemann <andre-kempf@mailbox.org>
2024-10-31 04:25:35 +0100

    feat: add random color initialization to AddContactComponent

--- src/components/main-content/contacts/add-contact/add-contact.tsx
+++ src/components/main-content/contacts/add-contact/add-contact.tsx
@@ -8,9 +8,39 @@ interface AddContactProps extends WithTranslation {
   closeDialog: () => void;
 }
 
-class AddContact extends Component<AddContactProps> {
+interface AddContactState {
+  currentColor: string;
+}
+
+class AddContact extends Component<AddContactProps, AddContactState> {
+  constructor(props: AddContactProps) {
+    super(props);
+    this.state = {
+      currentColor: this.getRandomColor(),
+    };
+  }
+
+  // Generating a random hex color
+  getRandomColor = (): string => {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
+  // Updating the color
+  updateColor = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedColor = event.target.value;
+    this.setState({
+      currentColor: selectedColor,
+    });
+  };
+
   render() {
     const { t, closeDialog } = this.props;
+    const { currentColor } = this.state;
 
     return (
       <div className="add-contact" onClick={closeDialog}>
@@ -27,7 +57,12 @@ class AddContact extends Component<AddContactProps> {
           <div className="add-contact-content">
             <div className="add-contact-badge">
               <div className="add-contact-color-picker">
-                <input type="color" />
+                <input
+                  type="color"
+                  value={currentColor}
+                  style={{ background: currentColor }}
+                  onChange={this.updateColor}
+                />
                 <div className="add-contact-initials"></div>
               </div>
             </div>
